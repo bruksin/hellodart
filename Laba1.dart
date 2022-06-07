eb535208c3358f60217d917aac5adcab3557fc70
Bruksin <ats@sl.ru>
2022-06-07 23:16:22 +0300

    Первый урок 1

--- Laba1.dart
+++ Laba1.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 class Bicycle {
   int cadence;
   int _speed = 0;
@@ -21,14 +23,54 @@ class Bicycle {
 
 class Summa {
   int a1, a2, a3;
-  int? sum=0;
 
   Summa(this.a1, this.a2, this.a3);
 
+  num get sum => a1+a2+a3;
 
   @override
-  String toString() => 'Summa : $sum';
+  String toString() => 'Summa : $sum $a1';
 }
+
+class Rectangle {
+  int width;
+  int height;
+  Point origin;
+
+  Rectangle({this.origin = const Point(0, 0), this.width = 0, this.height = 0});
+
+  @override
+  String toString() =>
+      'Origin: (${origin.x}, ${origin.y}), width: $width, height: $height';
+}
+
+abstract class Shape {
+  factory Shape(String type) {
+    if (type == 'circle') return Circle(2);
+    if (type == 'square') return Square(2);
+    throw 'Can\'t create $type.';
+  }
+  num get area;
+}
+
+class Circle implements Shape {
+  final num radius;
+  Circle(this.radius);
+  num get area => pi * pow(radius, 2);
+}
+
+class Square implements Shape {
+  final num side;
+  Square(this.side);
+  num get area => pow(side, 2);
+}
+
+class CircleMock implements Circle {
+  num area = 0;
+  num radius = 0;
+}
+
+String scream(int length) => "A${'a' * length}h!";
 
 void main() {
   var bike = Bicycle(2, 1);
@@ -36,4 +78,25 @@ void main() {
 
   var res = Summa(1, 2, 3);
   print(res);
+  print(res.sum);
+
+
+  print(Rectangle(origin: const Point(10, 20), width: 100, height: 200));
+  print(Rectangle(origin: const Point(10, 10)));
+  print(Rectangle(width: 200));
+  print(Rectangle());
+
+  final circle = Shape('circle');
+  final square = Shape('square');
+  print(circle.area);
+  print(square.area);
+
+  final values = [1, 2, 3, 5, 10, 50];
+  for (var length in values) {
+    print(scream(length));
+  }
+
+  values.map(scream).forEach(print);
+
+  values.skip(1).take(3).map(scream).forEach(print);
 }
